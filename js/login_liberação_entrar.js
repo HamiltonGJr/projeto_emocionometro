4a4b32456f591859c7b428c76dab38c9b566de68
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2023-09-29 20:17:11 -0300

    Fiz a parte de liberação do Login, caso os campos forem preenchidos corretamente.
    Obrigado Artur e Pedro que me ajudaram a fazer!!

---
+++ js/login_liberação_entrar.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('formulario')
+const botaoEntrar = document.getElementById('botaoEntrar')
+const email = document.getElementById('inputEmail')
+const senha = document.getElementById('inputSenha')
+
+
+function fazerLogin() {
+  if(email.value == '' && senha.value == '') {
+
+  } else {
+    window.location.href = '../home/index.html'
+  }
+}
+
+function verificarInput() {
+  if(email.value != '' && senha.value != '') {
+    botaoEntrar.classList.add('buttonSelecionado')
+    botaoEntrar.disabled = false
+  } else {
+    botaoEntrar.classList.remove('buttonSelecionado')
+    botaoEntrar.disabled = true
+  }
+}
+
+setInterval(verificarInput, 500)
