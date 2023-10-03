e8036d2905ac67a1be31d5986c0180c6fd097e0c
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2023-10-02 21:51:17 -0300

    JS - post.

---
+++ js/cadastro_aluno.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario')
+
+const cadastrarAluno = async (aluno) => {
+  await fetch('http://localhost:3000/aluno',{
+    method:'POST',  
+    headers:{
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(aluno)
+  })
+
+  window.location.href = '../aluno/index.html'
+}
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const nome = formulario.elements['name'].value
+  const turma = formulario.elements['turma'].value
+  const ativo = formulario.elements['ativo'].value
+
+  const aluno = {
+    nome,
+    turma,
+    ativo
+  }
+
+  cadastrarAluno(aluno)
+})
---
+++ js/cadastro_professor.js
@@ -0,0 +1,33 @@
+const formulario = document.getElementById('formulario')
+
+const cadastrarProfessor = async (professor) => {
+  await fetch('http://localhost:3000/professor',{
+    method:'POST',  
+    headers:{
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(professor)
+  })
+
+  window.location.href = '../professor/index.html'
+}
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const nome = formulario.elements['name'].value
+  const perfil = formulario.elements['perfil'].value
+  const disciplina = formulario.elements['disciplina'].value
+  const ativo = formulario.elements['ativo'].value
+
+  const professor = {
+    nome,
+    perfil,
+    disciplina,
+    ativo
+  }
+
+  cadastrarProfessor(professor)
+})
